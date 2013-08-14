bafbceb695d653fce624f4a1e139241c6ab898eb
Sean Copenhaver <sean.copenhaver@gmail.com>
2013-08-13 22:07:16 -0400

    dang it, syntax error in package file

--- package.exs
+++ package.exs
@@ -3,7 +3,7 @@ Expm.Package.new(
   description: "Rope in elixir providing faster index based operations then binaries.",
   version: "0.1.0",
   keywords: ["string", "data structure"],
-  licenses: ["Apache 2.0"]
+  licenses: ["Apache 2.0"],
   maintainers: [[name: "Sean Copenhaver", email: "sean.copenhaver+expm@gmail.com"]],
   repositories: [[github: "copenhas/ropex", tag: "v0.1.0"]]
 )
