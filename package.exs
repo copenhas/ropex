8f135106ad4be84c5a9580cfef980b5af969ef61
Sean Copenhaver <sean.copenhaver@gmail.com>
2013-08-13 21:59:32 -0400

    adding expm package file

---
+++ package.exs
@@ -0,0 +1,9 @@
+Expm.Package.new(
+  name: "ropex",
+  description: "Rope in elixir providing faster index based operations then binaries.",
+  version: "0.1.0",
+  keywords: ["string", "data structure"],
+  licenses: ["Apache 2.0"]
+  maintainers: [[name: "Sean Copenhaver", email: "sean.copenhaver+expm@gmail.com"]],
+  repositories: [[github: "copenhas/ropex", tag: "0.1.0"]]
+)
