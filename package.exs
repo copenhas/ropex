f25a4691dbd318d3d08083b22a6773d7352b160f
Sean Copenhaver <sean.copenhaver@gmail.com>
2013-08-14 08:45:46 -0400

    it did not like my licenses field

--- package.exs
+++ package.exs
@@ -3,7 +3,6 @@ Expm.Package.new(
   description: "Rope in elixir providing faster index based operations then binaries.",
   version: "0.1.0",
   keywords: ["string", "data structure"],
-  licenses: ["Apache 2.0"],
   maintainers: [[name: "Sean Copenhaver", email: "sean.copenhaver+expm@gmail.com"]],
   repositories: [[github: "copenhas/ropex", tag: "v0.1.0"]]
 )
