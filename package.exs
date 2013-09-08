48934f88b472a56e6f63dd691d84fc5f0ca5c89e
Sean Copenhaver <sean.copenhaver@gmail.com>
2013-09-07 22:13:50 -0400

    updating expm package to 0.1.1

--- package.exs
+++ package.exs
@@ -1,8 +1,8 @@
 Expm.Package.new(
   name: "ropex",
   description: "Rope in elixir providing faster index based operations then binaries.",
-  version: "0.1.0",
+  version: "0.1.1",
   keywords: ["string", "data structure"],
   maintainers: [[name: "Sean Copenhaver", email: "sean.copenhaver+expm@gmail.com"]],
-  repositories: [[github: "copenhas/ropex", tag: "v0.1.0"]]
+  repositories: [[github: "copenhas/ropex", tag: "v0.1.1"]]
 )
